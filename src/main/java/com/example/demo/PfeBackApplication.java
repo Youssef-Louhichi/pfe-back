7d0376882ee684d0651f6a5801efe88920b32ef2
Youssef <yousseflouhichi24@gmail.com>
2025-02-16 22:54:29 +0100

    prop configuration

--- src/main/java/com/example/demo/PfeBackApplication.java
+++ src/main/java/com/example/demo/PfeBackApplication.java
@@ -3,10 +3,18 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class PfeBackApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+        System.setProperty("url", dotenv.get("dburl"));
+        System.setProperty("username", dotenv.get("dbuser"));
+        System.setProperty("password", dotenv.get("dbpassword"));
+        System.setProperty("port", dotenv.get("dbport"));
+        
 		SpringApplication.run(PfeBackApplication.class, args);
 	}
 
